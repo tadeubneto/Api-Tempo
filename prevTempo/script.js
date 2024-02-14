512b3eafe78129985a0d642d1097bb70497ecc4f
Tadeu Neto <tadeubranconeto@gmail.com>
2024-02-14 11:20:23 -0300

    add P
    html - add um novo P para mostrar a condição do local
    css - ajustei a estilização do novo p
    js - fiz a converção da velocidade do vento
    fiz uma condição para que caso esteja chovendo, apareça a frase LEVE O GUARDA-CHUVA

--- prevTempo/script.js
+++ prevTempo/script.js
@@ -21,11 +21,20 @@ function getWeather(city) {
       ).innerText = `${data.name}, ${data.sys.country}`
       document.getElementById(
         'degrees'
-      ).innerText = `Temperatura: ${data.main.temp}°C`
+      ).innerText = `Temperatura: ${data.main.temp.toFixed(1)}°C`
+      //converti m/s para km/h
+      const speedInKm = data.wind.speed * 3.6
       document.getElementById(
         'wind'
-      ).innerText = `Velocidade do Vento: ${data.wind.speed} m/s`
-
+      ).innerText = `Velocidade do Vento: ${speedInKm.toFixed(1)} Km/h`
+      //avisar se está chovendo
+      const condicaoMet = data.weather[0].main;
+      document.getElementById(
+        'precipitation'
+      ).innerText = `${condicaoMet}`
+      if (condicaoMet.toLowerCase()=== 'rain') {
+        document.getElementById('precipitation').innerText = 'Leve o guarda-chuva!';
+      };
       // A URL da imagem do ícone do clima é fornecida pela API
       const weatherIcon = document.getElementById('weatherIcon')
       weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
@@ -35,6 +44,7 @@ function getWeather(city) {
       document.querySelectorAll('#content-response i').forEach(icon => {
         icon.classList.remove('hidden')
       })
+    
     })
     .catch(error => {
       console.error('Erro ao obter dados da API:', error)
